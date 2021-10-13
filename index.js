c35b7a6d4b1a982a59091b8b54cb5f5b7f233ac9
shreyaansjain <shreyaansjain60@gmail.com>
2021-10-13 17:52:18 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const data={
+    0: "-----",
+    1: ".----",
+    2: "..---",
+    3: "...--",
+    4: "....-",
+    5: ".....",
+    6: "-....",
+    7: "--...",
+    8: "---..",
+    9: "----.",
+    a: ".-",
+    b: "-...",
+    c: "-.-.",
+    d: "-..",
+    e: ".",
+    f: "..-.",
+    g: "--.",
+    h: "....",
+    i: "..",
+    j: ".---",
+    k: "-.-",
+    l: ".-..",
+    m: "--",
+    n: "-.",
+    o: "---",
+    p: ".--.",
+    q: "--.-",
+    r: ".-.",
+    s: "...",
+    t: "-",
+    u: "..-",
+    v: "...-",
+    w: ".--",
+    x: "-..-",
+    y: "-.--",
+    z: "--.."
+  }
+
+function texttomorse(){
+    const morsedata= document.getElementById("text").value ;
+   let morse =Object.values(morsedata)
+    const result= morse.map(function(e){
+        if(e==" "){
+            return '/'
+        }
+        return data[e];
+    })
+    alert(result.toString())
+}
